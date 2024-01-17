6131ba0d2a83408c9539ec23a7e2ed758a539e53
GerganaKraeva <gergana.evg.kraeva@gmail.com>
2024-01-17 20:50:04 +0200

    solutions

---
+++ Basic CRUD/Exercise/Exercise-tasks.sql
@@ -0,0 +1,23 @@
+-- 1. Find All Information About Departments
+-- Write a SQL query to find all available information about the departments. Sort the information by id
+
+SELECT*FROM departments 
+ORDER BY department_id;
+
+-- 2. Find all Department Names
+-- Write SQL query to find all department names. Sort the information by id
+
+SELECT name FROM departments
+ORDER BY department_id;
+
+-- 3. Find salary of Each Employee
+-- Write SQL query to find the first name, last name and salary of each employee. Sort the information by id
+
+SELECT first_name, last_name, salary FROM employees
+ORDER BY employee_id;
+
+-- 4. Find Full Name of Each Employee
+-- Write SQL query to find the first, middle and last name of each employee. Sort the information by id
+
+SELECT first_name, middle_name, last_name FROM employees
+ORDER BY employee_id;
