1ccc19f8e3424cf255d5c5e2cbd3beff448986f6
GerganaKraeva <gergana.evg.kraeva@gmail.com>
2024-01-18 00:05:51 +0200

    Update Exercise-tasks.sql

--- Basic CRUD/Exercise/Exercise-tasks.sql
+++ Basic CRUD/Exercise/Exercise-tasks.sql
@@ -57,3 +57,76 @@ WHERE salary IN(25000,14000,12500,23600) ;
 
 SELECT first_name, last_name FROM employees
 WHERE manager_id IS NULL;
+-- 11. Find All Employees with salary More Than 50000
+-- Write a SQL query to find first name, last name and salary of those employees who has salary more than 50000. Order them in decreasing order by salary.
+
+SELECT first_name, last_name, salary FROM employees
+WHERE salary>50000
+ORDER BY salary DESC;
+
+-- 12. Find 5 Best Paid Employees
+-- Write SQL query to find first and last names about 5 best paid Employees ordered descending by their salary.
+
+SELECT first_name, last_name FROM employees
+ORDER BY salary DESC
+lIMIT 5;
+
+-- 13. Find All Employees Except Marketing
+-- Write a SQL query to find the first and last names of all employees whose department ID is different from 4
+
+SELECT first_name, last_name FROM employees
+WHERE department_id NOT IN(4);
+
+
+-- 14. Sort Employees Table
+-- Write a SQL query to sort all records in the еmployees table by the following criteria:
+-- • First by salary in decreasing order
+-- • Then by first name alphabetically
+-- • Then by last name descending
+-- • Then by middle name alphabetically
+-- Sort the information by id
+
+SELECT*FROM employees
+ORDER BY salary DESC,
+          first_name,
+          last_name DESC,
+          middle_name,
+          employee_id DESC;
+          
+-- 15. Create View Employees with Salaries
+-- Write a SQL query to create a view v_employees_salaries with first name, last name and salary for each employee
+
+CREATE VIEW v_employees_salaries AS
+SELECT first_name, last_name, salary FROM employees;
+
+-- 16. Create View Employees with Job Titles
+-- Write a SQL query to create view v_employees_job_titles with full employee name and job title. When middle name is NULL replace it with empty string ('')
+
+CREATE VIEW v_employees_job_titles AS
+SELECT CONCAT_WS(' ',first_name, middle_name, last_name) AS 'full_name',
+       job_title
+       FROM employees;
+       
+-- 17. Distinct Job Titles
+-- Write a SQL query to find all distinct job titles. Sort the result by job title alphabetically.
+
+SELECT DISTINCT job_title FROM employees
+ORDER BY job_title;
+
+-- 18. Find First 10 Started Projects
+-- Write a SQL query to find first 10 started projects. Select all information about them and sort them by start date, then by name. Sort the information by id
+
+SELECT*FROM projects
+ORDER BY start_date,
+         name,
+         project_id
+LIMIT 10;
+
+-- 19. Last 7 Hired Employees
+-- Write a SQL query to find last 7 hired employees. Select their first, last name and their hire date
+
+
+SELECT first_name, last_name, hire_date FROM employees
+ORDER BY hire_date DESC
+LIMIT 7;
+
