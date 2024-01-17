d673046ba0dffa8c4112afd2b9d9e4697fbbf47a
GerganaKraeva <gergana.evg.kraeva@gmail.com>
2024-01-18 00:04:32 +0200

    Update Exercise-tasks.sql

--- Basic CRUD/Exercise/Exercise-tasks.sql
+++ Basic CRUD/Exercise/Exercise-tasks.sql
@@ -21,3 +21,39 @@ ORDER BY employee_id;
 
 SELECT first_name, middle_name, last_name FROM employees
 ORDER BY employee_id;
+
+
+-- 5. Find Email Address of Each Employee
+-- Write a SQL query to find the email address of each employee. (by his first and last name). Consider that the email domain is softuni.bg. Emails should look like "John.Doe@softuni.bg". The produced column should be named "full_ email_address".
+
+SELECT CONCAT(first_name,'.',last_name,'@softuni.bg')  AS 'full_ email_address' FROM employees;
+
+-- 6. Find All Different Employee's Salaries
+-- Write a SQL query to find all different employee's salaries
+
+SELECT DISTINCT salary FROM employees;
+
+-- 7. Find all Information About Employees
+-- Write a SQL query to find all information about the employees whose job title is "Sales Representative". Sort the information by id.employees
+
+SELECT*FROM employees
+WHERE job_title = 'Sales Representative'
+ORDER BY employee_id;
+
+-- 8. Find Names of All Employees by salary in Range
+-- Write a SQL query to find the first name, last name and job title of all employees whose salary is in the range [20000, 30000]. Sort the information by id.
+
+SELECT first_name,last_name,job_title FROM employees
+WHERE salary BETWEEN 20000 AND 30000;
+
+-- 9. Find Names of All Employees
+-- Write a SQL query to find the full name of all employees whose salary is 25000, 14000, 12500 or 23600. Full Name is combination of first, middle and last name (separated with single space) and they should be in one column called "Full Name".
+SELECT 
+CONCAT_WS(' ',first_name,middle_name,last_name) AS 'Full Name' FROM employees
+WHERE salary IN(25000,14000,12500,23600) ;
+
+-- 10. Find All Employees Without Manager
+-- Write a SQL query to find first and last names about those employees that does not have a manager
+
+SELECT first_name, last_name FROM employees
+WHERE manager_id IS NULL;
