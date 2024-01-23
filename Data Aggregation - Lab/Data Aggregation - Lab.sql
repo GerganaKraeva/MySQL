d816aef1411ed126a9581823f38a3cdd05211405
GerganaKraeva <gergana.evg.kraeva@gmail.com>
2024-01-23 15:04:06 +0200

    Update Data Aggregation - Lab.sql

--- Data Aggregation - Lab/Data Aggregation - Lab.sql
+++ Data Aggregation - Lab/Data Aggregation - Lab.sql
@@ -12,3 +12,34 @@ SELECT department_id, ROUND(AVG(salary),2) AS 'Average Salary'
 FROM employees
 GROUP BY  department_id
 ORDER BY department_id;
+
+-- 3. Min Salary
+-- Write a query to retrieve information about the departments grouped by department_id with minimum salary higher than 800. Round the salary result to two digits after the decimal point. Submit your queries with the MySQL prepare DB & run queries strategy.
+
+SELECT 
+department_id,
+MIN(salary) AS `Min Salary`
+FROM employees
+GROUP BY department_id
+HAVING `Min Salary` > 800;
+
+-- 4. Appetizers Count
+-- Write a query to retrieve the count of all appetizers (category id = 2) with price higher than 8
+
+SELECT  COUNT(*) FROM products
+WHERE category_id=2 AND price > 8;
+
+-- 5. Menu Prices
+-- Write a query to retrieve information about the prices of each category. The output should consist of:
+-- · Category_id
+-- · Average Price
+-- · Cheapest Product
+-- · Most Expensive Product
+-- See the examples for more information. Round the results to 2 digits after the decimal poin
+
+SELECT category_id, 
+ROUND(AVG(price),2) AS `Average Price`,
+ROUND(MIN(price),2) AS `Cheapest Product`,
+ROUND(max(price),2) AS `Most Expensive Product`
+FROM products
+GROUP BY category_id;
